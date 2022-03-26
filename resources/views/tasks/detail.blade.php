78d58add788724f18d60d7cf91626cbb88720c94
Martin <martinzboril1@hotmail.com>
2022-03-26 18:05:40 +0100

    Update detail.blade.php

--- resources/views/tasks/detail.blade.php
+++ resources/views/tasks/detail.blade.php
@@ -23,7 +23,42 @@
             <div class="row">
                 <div class="col-md-5">
                     <div class="card card-primary card-outline rounded-0">
-
+                        <div class="card-header">{{ $task->name }}</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12 col-sm-4">
+                                    <div class="info-box bg-light">
+                                        <div class="info-box-content">
+                                            <span class="info-box-text text-center text-muted">Due date</span>
+                                            <span class="info-box-number text-center text-muted mb-0"><span class="badge badge-danger">{{ $task->due_date->format('d.m.Y') }}</span></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-4">
+                                    <div class="info-box bg-light">
+                                        <div class="info-box-content">
+                                            <span class="info-box-text text-center text-muted">Start date</span>
+                                            <span class="info-box-number text-center text-muted mb-0"><span class="badge badge-success">{{ $task->start_date->format('d.m.Y') }}</span></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-4">
+                                    <div class="info-box bg-light">
+                                        <div class="info-box-content">
+                                            <span class="info-box-text text-center text-muted">Created at</span>
+                                            <span class="info-box-number text-center text-muted mb-0"><span class="badge badge-secondary">{{ $task->created_at->format('d.m.Y') }}</span></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="d-block">Project: <b>{{ $task->project->name }}</b></span>
+                            <span class="d-block">User: <b>{{ $task->user->name }} {{ $task->user->surname }}</b></span>
+                            @if ($task->user->id != $task->author->id)
+                                <span class="d-block">Author: <b>{{ $task->author->name }} {{ $task->author->surname }}</b></span>
+                            @endif
+                            <hr>
+                            {!! $task->description !!}
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-7">
