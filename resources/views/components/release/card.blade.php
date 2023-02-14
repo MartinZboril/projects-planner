252ce7dc82a326105d020bfd82f3cb000d6ba7d8
webBook <martinzboril1@hotmail.com>
2023-02-14 12:13:35 +0100

    add components for releases

---
+++ resources/views/components/release/card.blade.php
@@ -0,0 +1,15 @@
+<div class="card card-primary card-outline">
+    <div class="card-header">Releases</div>
+    <div class="card-body">
+        <!-- Content -->
+        @if ($releases->get('releases')->count() > 0)
+            <div class="timeline">
+                @foreach ($releases->get('releases')->sortByDesc('realese_date') as $key => $release)
+                    <x-release.item :$release :$key />
+                @endforeach
+            </div>                            
+        @else
+            No releases were found!
+        @endif
+    </div>
+</div>            
