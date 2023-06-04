6134194e147679c166d8df640d7def1f45238b94
MartinZboril <martinzboril1@hotmail.com>
2023-06-04 19:24:54 +0200

    complete basic seeder

---
+++ database/factories/RateFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rate>
+ */
+class RateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'value' => $this->faker->numberBetween(100, 1000),
+            'is_active' => true,     
+            'note' => $this->faker->paragraph(),       
+        ];
+    }
+}
