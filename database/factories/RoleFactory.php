f312e8eb28e746256c4f77c58d3c9c5860a35a64
MartinZboril <martinzboril1@hotmail.com>
2023-06-19 18:11:49 +0200

    make feature tests for RoleTest

---
+++ database/factories/RoleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\odel=Role>
+ */
+class RoleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'is_active' => true,
+            'note' => fake()->paragraph(),
+        ];
+    }
+}
