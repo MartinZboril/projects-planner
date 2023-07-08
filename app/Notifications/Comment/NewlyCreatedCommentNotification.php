faacb5e1ee89646c9052f111b3cc1b67aee59e83
MartinZboril <martinzboril1@hotmail.com>
2023-07-08 13:34:21 +0200

    Update NewlyCreatedCommentNotification.php

--- app/Notifications/Comment/NewlyCreatedCommentNotification.php
+++ app/Notifications/Comment/NewlyCreatedCommentNotification.php
@@ -28,7 +28,7 @@ class NewlyCreatedCommentNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
