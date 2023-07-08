9c97ba040e7ef756da74e6b1da2c4b110190ea58
MartinZboril <martinzboril1@hotmail.com>
2023-07-08 20:27:33 +0200

    removed notification item into component

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -20,23 +20,7 @@
                     </div>
                     <div class="card-body">
                         @forelse ($notifications as $notification)
-                            <li class="list-group-item notification-{{ $notification->id }}-item">
-                                <div class="text-muted">
-                                    <i class="fas fa-clock"></i>
-                                    {{ $notification->created_at->diffForHumans() }}
-                                    <span class="float-right">
-                                        @if ($notification->data['link'] ?? false)
-                                            <a href="#" onclick="viewNotificationLink('{{ route('notifications.seen', $notification) }}', '{{ $notification->data['link'] }}')" title="Detail">
-                                                <i class="fas fa-eye mr-1"></i>
-                                            </a>
-                                        @endif
-                                        <a href="#" onclick="seenNotification('{{ route('notifications.seen', $notification) }}', '{{ $notification->id }}')" class="text-success" title="Mark as read">
-                                            <i class="fas fa-book-open mr-1"></i>
-                                        </a>
-                                    </span>
-                                </div>
-                                {{ $notification->data['content'] }}
-                            </li>
+                            <x-notification.item :$notification />
                         @empty
                             No notifications were recieved!
                         @endforelse
